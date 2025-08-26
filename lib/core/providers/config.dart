b71107f65f584f7ce544f6587091e62d0fcfea4c
SPiercer <snowpiercer2001@gmail.com>
2025-08-26 19:13:55 +0300

    Refactor configProvider to return a Config object; add Config class to manage environment variables

--- lib/core/providers/config.dart
+++ lib/core/providers/config.dart
@@ -3,5 +3,12 @@ import 'package:dotenv/dotenv.dart';
 
 Middleware configProvider() {
   final env = DotEnv(includePlatformEnvironment: true)..load();
-  return provider<DotEnv>((context) => env);
+  return provider<Config>((context) => Config(env));
+}
+
+class Config {
+  Config(this._env);
+  final DotEnv _env;
+
+  String? get geminiApiKey => _env['GEMINI_API_KEY'];
 }
